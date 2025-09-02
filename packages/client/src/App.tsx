ce6ec0ab97f512449a099eb8e8fd9230c8d016ae
mv <madhuvadde237@gmail.com>
2025-09-02 21:42:44 +0530

    Tailwindcss setup

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       .then((res) => res.json())
       .then((data) => setmessage(data.message));
   }, []);
-  return <p>{message}</p>;
+  return <p className="font-bold p-4">{message}</p>;
 }
 
 export default App;
