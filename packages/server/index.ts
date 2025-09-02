239dc80d0d8aedf3aad23fb67254fa22138c8b2c
mv <madhuvadde237@gmail.com>
2025-09-02 21:28:10 +0530

    connect the frontend and backend

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -9,6 +9,9 @@ const port = process.env.PORT || 3000;
 app.get("/", (req: Request, res: Response) => {
   res.send(`Hello World!! ${process.env.HUGGING_API_KEY}`);
 });
+app.get("/api/hello", (req: Request, res: Response) => {
+  res.send({message: `Hello World of OG!!`});
+});
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
