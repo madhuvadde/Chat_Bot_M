c41b0ab76f278fa5b2c57b4f096f1b137b7dc5cb
mv <madhuvadde237@gmail.com>
2025-09-03 09:58:22 +0530

    prettier setup done

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,20 +1,20 @@
-import { useState, useEffect } from "react";
-import "./App.css";
-import { Button } from "./components/ui/button";
+import { useState, useEffect } from 'react';
+import './App.css';
+import { Button } from './components/ui/button';
 
 function App() {
-  const [message, setmessage] = useState("");
-  useEffect(() => {
-    fetch("/api/hello")
-      .then((res) => res.json())
-      .then((data) => setmessage(data.message));
-  }, []);
-  return (
-    <div className="p-4">
-      <p className="font-bold text-3xl">{message}</p>
-      <Button>They Call Him OG</Button>
-    </div>
-  );
+   const [message, setmessage] = useState('');
+   useEffect(() => {
+      fetch('/api/hello')
+         .then((res) => res.json())
+         .then((data) => setmessage(data.message));
+   }, []);
+   return (
+      <div className="p-4">
+         <p className="font-bold text-3xl">{message}</p>
+         <Button>They Call Him OG</Button>
+      </div>
+   );
 }
 
 export default App;
--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,18 +1,18 @@
-import express from "express";
-import type { Request, Response }from "express";    
-import dotenv from "dotenv";
+import express from 'express';
+import type { Request, Response } from 'express';
+import dotenv from 'dotenv';
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get("/", (req: Request, res: Response) => {
-  res.send(`Hello World!! ${process.env.HUGGING_API_KEY}`);
+app.get('/', (req: Request, res: Response) => {
+   res.send(`Hello World!! ${process.env.HUGGING_API_KEY}`);
 });
-app.get("/api/hello", (req: Request, res: Response) => {
-  res.send({message: `Hello World of OG!!`});
+app.get('/api/hello', (req: Request, res: Response) => {
+   res.send({ message: `Hello World of OG!!` });
 });
 
 app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+   console.log(`Server is running on http://localhost:${port}`);
 });
