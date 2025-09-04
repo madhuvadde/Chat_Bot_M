e490179dd7b6371909f84c482e7524f01c2cccb6
mv <madhuvadde237@gmail.com>
2025-09-04 19:41:00 +0530

    zod validation and error handling added

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import type { Request, Response } from 'express';
 import dotenv from 'dotenv';
 import { llmClient } from './llm/client';
+import z from 'zod';
 dotenv.config();
 
 const app = express();
@@ -20,11 +21,28 @@ app.post('/api/summarize', async (req: Request, res: Response) => {
    const summary = await llmClient.summarize(text);
    res.send({ summary });
 });
-let lastChatId: string | null = null;
+const chatSchema = z.object({
+   prompt: z
+      .string()
+      .trim()
+      .min(1, 'Prompt is required')
+      .max(100, 'Prompt is too long(max 100 characters'),
+   conversationId: z.uuid(),
+});
 app.post('/api/chat', async (req: Request, res: Response) => {
-   const { prompt, conversationId } = req.body;
-   const { message } = await llmClient.generate(prompt, conversationId);
-   res.send({ message });
+   const parseResult = chatSchema.safeParse(req.body);
+   if (!parseResult.success) {
+      const errors = z.treeifyError(parseResult.error);
+      res.status(400).send(errors);
+      return;
+   }
+   try {
+      const { prompt, conversationId } = req.body;
+      const { message } = await llmClient.generate(prompt, conversationId);
+      res.send({ message });
+   } catch (error) {
+      res.status(500).send({ error: `Failed to generate a response.` });
+   }
 });
 
 app.listen(port, () => {
