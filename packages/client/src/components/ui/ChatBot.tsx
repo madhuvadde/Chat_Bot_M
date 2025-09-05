b58d05947ee7be739a31456a415f29950fc28f2a
mv <madhuvadde237@gmail.com>
2025-09-05 17:34:20 +0530

    Implemented button disabled, on enter key form submit, react hook form submission

--- packages/client/src/components/ui/ChatBot.tsx
+++ packages/client/src/components/ui/ChatBot.tsx
@@ -1,18 +1,45 @@
+import type { KeyboardEvent } from 'react';
 import { Button } from './button';
 import { FaArrowUp } from 'react-icons/fa6';
+import { useForm } from 'react-hook-form';
+
+type FormData = {
+   prompt: string;
+};
 
 const ChatBot = () => {
+   const { register, handleSubmit, reset, formState } = useForm<FormData>();
+   const onSubmitHandler = (data: FormData) => {
+      console.log(data);
+      reset();
+   };
+
+   const onKeyDownHandler = (e: KeyboardEvent<HTMLFormElement>) => {
+      if (e.key === 'Enter' && !e.shiftKey) {
+         e.preventDefault(); //To get rid of extra space when clicked on enter(default behaviour)
+         handleSubmit(onSubmitHandler)();
+      }
+   };
+
    return (
-      <div className="flex flex-col gap-2 items-end border-2 p-4 rounded-3xl">
+      <form
+         onSubmit={handleSubmit(onSubmitHandler)}
+         className="flex flex-col gap-2 items-end border-2 p-4 rounded-3xl"
+         onKeyDown={onKeyDownHandler}
+      >
          <textarea
+            {...register('prompt', {
+               required: true,
+               validate: (data) => data.trim().length > 0,
+            })}
             className="w-full border-0 focus:outline-0 resize-none"
             placeholder="Ask anything"
             maxLength={100}
          />
-         <Button className="rounded-full w-9 h-9">
+         <Button disabled={!formState.isValid} className="rounded-full w-9 h-9">
             <FaArrowUp />
          </Button>
-      </div>
+      </form>
    );
 };
 
