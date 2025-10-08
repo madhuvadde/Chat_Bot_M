a18dc537dc5eb8560cea1ad13692164b986f157e
madhuvadde <madhuvadde237@gmail.com>
2025-10-08 07:16:48 +0530

    displaying skeleton loaders

--- packages/client/src/components/reviews/ReviewList.tsx
+++ packages/client/src/components/reviews/ReviewList.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 import axios from 'axios';
 import StarRatings from './StarRatings';
+import Skeleton from 'react-loading-skeleton';
 
 type Props = {
    productId: number;
@@ -20,15 +21,33 @@ type GetReviewsResponse = {
 };
 const ReviewList = ({ productId }: Props) => {
    const [reviewData, setReviewData] = useState<GetReviewsResponse>();
+   const [isLoading, setIsLoading] = useState<boolean>(false);
    const fetchReviews = async () => {
+      setIsLoading(true);
       const { data } = await axios.get<GetReviewsResponse>(
          `/api/products/${productId}/reviews`
       );
       setReviewData(data);
+      setIsLoading(false);
    };
+
    useEffect(() => {
       fetchReviews();
    }, []);
+
+   if (isLoading) {
+      return (
+         <div className="flex flex-col gap-5">
+            {[1, 2, 3].map((i) => (
+               <div key={i}>
+                  <Skeleton width={150} />
+                  <Skeleton width={100} />
+                  <Skeleton width={2} />
+               </div>
+            ))}
+         </div>
+      );
+   }
    return (
       <div className="flex flex-col gap-5">
          {reviewData?.reviews.map((review) => (
